f7dab86f13a9314a9417bdfcc679c2dfdaad3507
joaoptgaino <joaoptgaino@hotmail.com>
2020-08-17 19:44:27 -0300

    Now you can add new products

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 import {useHistory} from 'react-router-dom';
 export interface Product {
@@ -10,12 +10,13 @@ interface ProductItemProps {
     product: Product;
 }
 const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
-    const history = useHistory();
     function deleteProduct(){
         return api.delete(`products/${product.id}`)
         .then(response=>{
             alert("Deleted product");
-            history.push('/list');
+        })
+        .catch(err=>{
+            console.log(err);
         })
         
     }
--- src/pages/ProductForm/index.tsx
+++ src/pages/ProductForm/index.tsx
@@ -1,23 +1,40 @@
-import React,{FormEvent, useState} from 'react';
+import React, { FormEvent, useState } from 'react';
+import api from '../../services/api';
+import { useHistory } from 'react-router-dom';
 
-function ProductForm(){
+function ProductForm() {
+    const history = useHistory();
+    const [name, setName] = useState('');
+    const [price, setPrice] = useState('');
+    function handleCreateProduct(e: FormEvent) {
+        e.preventDefault();
 
-    const [name,setName] = useState('');
-    function handleCreateProduct(e:FormEvent){
-        e.preventDefault
+        api.post('products',{
+            name,
+            price:Number(price)
+        }).then(()=>{
+            alert("Created product");
+            history.push('/');
+        }).catch(()=>{
+            alert("Error while creating product");
+        })
     }
-    return(
+    return (
         <div id="product-form" className="container">
             <h1>Product Form</h1>
-        <main>
-            <form onSubmit={handleCreateProduct}>
-                <input type="text" name="name" placeholder="Name" value={name}
-                onChange={(e)=>{}}
-                />
-            </form>
-        </main>
+            <main>
+                <form onSubmit={handleCreateProduct}>
+                    <input type="text" name="name" placeholder="Name" value={name}
+                        onChange={(e) => { setName(e.target.value) }}
+                    />
+                    <input type="text" name="price" placeholder="Price" value={price}
+                        onChange={(e) => { setPrice(e.target.value) }}
+                    />
+                    <button type="submit">Criar</button>
+                </form>
+            </main>
         </div>
     );
-}  
+}
 
 export default ProductForm;
