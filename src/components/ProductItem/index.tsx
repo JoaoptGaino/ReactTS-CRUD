f0a618c6d95e863b806e7e8df3b1fb4162f8fbe4
joaoptgaino <joaoptgaino@hotmail.com>
2020-08-17 00:06:23 -0300

    Started working in the product form

--- src/components/ProductItem/index.tsx
+++ src/components/ProductItem/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import api from '../../services/api';
-
+import {useHistory} from 'react-router-dom';
 export interface Product {
     id: number;
     name: string;
@@ -10,8 +10,14 @@ interface ProductItemProps {
     product: Product;
 }
 const ProductItem: React.FC<ProductItemProps> = ({ product }) => {
+    const history = useHistory();
     function deleteProduct(){
-        api.delete(`products/${product.id}`)
+        return api.delete(`products/${product.id}`)
+        .then(response=>{
+            alert("Deleted product");
+            history.push('/list');
+        })
+        
     }
     return (
         <article className="product-item">
--- src/pages/ProductForm/index.tsx
+++ src/pages/ProductForm/index.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React,{FormEvent, useState} from 'react';
 
 function ProductForm(){
+
+    const [name,setName] = useState('');
+    function handleCreateProduct(e:FormEvent){
+        e.preventDefault
+    }
     return(
-        <h1>Product Form</h1>
+        <div id="product-form" className="container">
+            <h1>Product Form</h1>
+        <main>
+            <form onSubmit={handleCreateProduct}>
+                <input type="text" name="name" placeholder="Name" value={name}
+                onChange={(e)=>{}}
+                />
+            </form>
+        </main>
+        </div>
     );
 }  
 
